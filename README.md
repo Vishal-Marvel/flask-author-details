e03fc914d99b4eb8ff91e69c1bb80e64bd34931d
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2023-06-04 21:10:17 +0530

    Completed

---
+++ README.md
@@ -0,0 +1,11 @@
+# Author Details
+* Download the code `git clone `
+* Open cmd in the downloaded folder and run `pip install -r requirements.txt`
+* Configure line 9 in app.py based on your database  
+* Create the database in the mysql no the **tables**
+* Configure the models defined in line 17 and 23 of `app.py` with your preference
+* In the cmd run `flask db init`
+
+* Then `flask db migrate -m "Initial"`
+* Then `flask db upgrade`
+* Then `flask run`
--- app.py
+++ app.py
@@ -1,13 +1,36 @@
 from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Change this to a secure secret key
 
-# Sample data for authors
-authors = [
-    {'id': 1, 'name': 'John Doe', 'email': 'john@example.com'},
-    {'id': 2, 'name': 'Jane Smith', 'email': 'jane@example.com'}
-]
+# SQLAlchemy configurations
+app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://your_username:your_databse_password@localhost/your_database_name'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+# Author model
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    contact_no = db.Column(db.String(20), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    linkedin = db.Column(db.String(100), nullable=False)
+    google_plus = db.Column(db.String(100), nullable=False)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 # Login route
 @app.route('/login', methods=['GET', 'POST'])
@@ -16,11 +39,15 @@ def login():
         username = request.form['username']
         password = request.form['password']
         
-        # Add your authentication logic here
-        # For simplicity, let's assume authentication is successful
-        session['username'] = username
-        return redirect('/author-details')
-    
+        # Perform login authentication
+        user = User.query.filter_by(username=username, password=password).first()
+
+        if user:
+            session['username'] = user.username
+            return redirect('/')
+        else:
+            return render_template('login.html', error='Invalid credentials')
+
     return render_template('login.html')
 
 # Logout route
@@ -28,6 +55,23 @@ def login():
 def logout():
     session.pop('username', None)
     return redirect('/login')
+
+# Register route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Insert new user into the database
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        session['username'] = username
+        return redirect('/author-details')
+
+    return render_template('register.html')
 
 # Author details entry route
 @app.route('/author-details', methods=['GET', 'POST'])
@@ -37,10 +81,16 @@ def author_details():
 
     if request.method == 'POST':
         name = request.form['name']
-        email = request.form['email']
+        contact_no = request.form['contact_no']
+        address = request.form['address']
+        linkedin = request.form['linkedin']
+        google_plus = request.form['google_plus']
         
-        # Add your database insertion logic here
-        
+        # Insert new author details into the database
+        new_author = Author(name=name, contact_no=contact_no, address=address, linkedin=linkedin, google_plus=google_plus)
+        db.session.add(new_author)
+        db.session.commit()
+
         return redirect('/author-details')
 
     return render_template('author_details.html')
@@ -54,12 +104,11 @@ def details_search():
     if request.method == 'POST':
         search_term = request.form['search_term']
         
-        # Add your database search logic here
-        # For simplicity, let's assume the search returns a list of authors
-        search_results = [author for author in authors if search_term.lower() in author['name'].lower()]
+        # Search for authors in the database
+        search_results = Author.query.filter(Author.name.like('%' + search_term + '%')).all()
         
         return render_template('details_search.html', search_results=search_results)
-
+    
     return render_template('details_search.html')
 
 
